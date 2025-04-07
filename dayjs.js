48609265aa97c2584c1f8d6d3be928b4b13735d3
kdrv dev <ffeodalik@gmail.com>
2025-04-07 12:51:11 +0500

    Update qilib boriladi

--- dayjs.js
+++
@@ -1,7 +0,0 @@
-import dayjs from "dayjs";
-
-
-console.log(dayjs().format()); 
-console.log(dayjs().format('YYYY-MM-DD'));
-console.log(dayjs().format('DD/MM/YYYY'));
-console.log(dayjs().format('HH:mm:ss'));
